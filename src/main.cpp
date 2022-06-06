4e65733c0dd81ac07254f930d801b99ee1decef9
lstpc <94103092+lstpc@users.noreply.github.com>
2022-06-06 23:26:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,31 @@
-#include "lib/TAObjects.h"
+#include "lib/class.h"
 
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
 
  
 int main() {
-    // ебать я создал абсрактный хеликоптер
-
-    TAircraft craft(0, 0, 2, 100, 100);
-
-    for (int i = 0; i < 40; i++) {
-
-        craft.calculate();
-        int a1 = craft.calculate_a(50, false);
-        craft.move(10, a1);
-        craft.update_landing(100);
-
-        printf("%i (%f %f) %d \n", a1, craft.x, craft.y, craft.landing);
-        
-        if (craft.landing) {
-            break;
-        }
+    TAirport airp1(0.0, 0.0, 10.0);
+    TLA* TNP[4];
+    // целых 4 абстрактынх хеликоптера
+    THelicopter Hel1(0.0, 0.0, 10.0, 100.0, 100.0); TNP[0] = &Hel1;
+    THelicopter Hel2(0.0, 0.0, 10.0, 100.0, -100.0); TNP[1] = &Hel2;
+    THelicopter Hel3(0.0, 0.0, 10.0, -100.0, -100.0); TNP[2] = &Hel3;
+    THelicopter Hel4(0.0, 0.0, 10.0, -100.0, 100.0); TNP[3] = &Hel4;
+    for (int i =0; i < 4; i++ ) {
+        airp1.LA.push_back(TNP[i]);
+        airp1.LA.push_back(TNP[i]);
+        airp1.LA.push_back(TNP[i]);
+        airp1.LA.push_back(TNP[i]);
     }
+    airp1.Do(0.0, 15.0);
+    // int a1 = 0; // craft.calculate_a(100, false);
+
+    // airp1.Do(0, 10);
+    //craft.update_landing(100);
+
+    //cout << a1 << ", " << Hel1.xc << ", " << Hel1.yc << endl; //", " << craft.landing << ", " << craft.R << endl;
 
     return 0;
 }
