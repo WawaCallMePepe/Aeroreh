4070a09d32f505f66fa0053c16b6ee6f9249ad26
Negor <alienlord9280@gmail.com>
2022-06-07 01:41:29 +0500

    передел_очка
    Ну ты и надристал конечно

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,21 @@ using namespace std;
  
 int main() {
     TAirport airp1(0.0, 0.0, 10.0);
-    TLA* TNP[4];
+	
     // целых 4 абстрактынх хеликоптера
-    THelicopter Hel1(0.0, 0.0, 10.0, 100.0, 100.0); TNP[0] = &Hel1;
-    THelicopter Hel2(0.0, 0.0, 10.0, 100.0, -100.0); TNP[1] = &Hel2;
-    THelicopter Hel3(0.0, 0.0, 10.0, -100.0, -100.0); TNP[2] = &Hel3;
-    THelicopter Hel4(0.0, 0.0, 10.0, -100.0, 100.0); TNP[3] = &Hel4;
-    for (int i =0; i < 4; i++ ) {
-        airp1.LA.push_back(TNP[i]);
-        airp1.LA.push_back(TNP[i]);
-        airp1.LA.push_back(TNP[i]);
-        airp1.LA.push_back(TNP[i]);
-    }
+    THelicopter* Hel1 = new THelicopter(0.0, 0.0, 10.0, 100.0, 100.0); 
+    THelicopter* Hel2 = new THelicopter(0.0, 0.0, 10.0, 100.0, -100.0); 
+    THelicopter* Hel3 = new THelicopter(0.0, 0.0, 10.0, -100.0, -100.0);     
+	THelicopter* Hel4 = new THelicopter(0.0, 0.0, 10.0, -100.0, 100.0); 
+	
+    
+    airp1.LA.push_back(hel1);
+	airp1.LA.push_back(hel2);
+	airp1.LA.push_back(hel3);
+	airp1.LA.push_back(hel4);
+    
+	
     airp1.Do(0.0, 15.0);
-    // int a1 = 0; // craft.calculate_a(100, false);
-
-    // airp1.Do(0, 10);
-    //craft.update_landing(100);
-
-    //cout << a1 << ", " << Hel1.xc << ", " << Hel1.yc << endl; //", " << craft.landing << ", " << craft.R << endl;
 
     return 0;
 }
